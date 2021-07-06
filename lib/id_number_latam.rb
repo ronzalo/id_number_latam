578e82bfef3371fcab6a02ad12a9d25e1fad0c23
Gonzalo Moreno <goncab380@hotmail.com>
2021-07-06 19:16:16 -0400

    first commit

---
+++ lib/id_number_latam.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "id_number_latam/version"
+
+module IdNumberLatam
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/id_number_latam_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe IdNumberLatam do
+  it "has a version number" do
+    expect(IdNumberLatam::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
