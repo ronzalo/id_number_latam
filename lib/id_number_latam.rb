ca752630727aa70330eb834921eed3f5a238cac2
Gonzalo Moreno <goncab380@hotmail.com>
2021-07-20 22:09:50 -0400

    add peruvian dni

--- lib/id_number_latam.rb
+++ lib/id_number_latam.rb
@@ -4,6 +4,7 @@ require_relative "id_number_latam/version"
 require_relative "id_number_latam/base"
 require_relative "id_number_latam/cl_dni"
 require_relative "id_number_latam/ec_dni"
+require_relative "id_number_latam/pe_dni"
 require_relative "id_number_latam/uy_dni"
 
 module IdNumberLatam
